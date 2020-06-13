b2b8b7b919ae15864c00c652c2d050d3f3da441a
MinexAutomation <minex.automation@gmail.com>
2020-06-13 13:05:12 -0700

    Added R5T.D0010.IMessageSink input parameter to IFileEqualityComparer.Equals() method.

--- source/R5T.D0015.Base/Code/Services/IFileEqualityComparer.cs
+++ source/R5T.D0015.Base/Code/Services/IFileEqualityComparer.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Threading.Tasks;
 
+using R5T.D0010;
+
 
 namespace R5T.D0015
 {
     public interface IFileEqualityComparer
     {
-        Task Equals(string filePath1, string filePath2);
+        Task Equals(string filePath1, string filePath2, IMessageSink messageSink);
     }
 }
