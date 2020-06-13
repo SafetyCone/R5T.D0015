55d96f2f38cae56bec41b3cc844d1a811600d345
MinexAutomation <minex.automation@gmail.com>
2020-06-13 13:02:57 -0700

    Added R5T.D0015.Base service definition library and IFileEqualityComparer service definition.

---
+++ source/R5T.D0015.Base/Code/Services/IFileEqualityComparer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0015
+{
+    public interface IFileEqualityComparer
+    {
+        Task Equals(string filePath1, string filePath2);
+    }
+}
