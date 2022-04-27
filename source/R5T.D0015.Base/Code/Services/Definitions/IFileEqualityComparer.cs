e8e219e3bde151984d25a9822abd78d35007e15e
davidcoats <david@getsafetycone.com>
2022-04-27 15:26:43 -0700

    Interim changes.

---
+++ source/R5T.D0015.Base/Code/Services/Definitions/IFileEqualityComparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.D0010;
+using R5T.T0064;
+
+
+namespace R5T.D0015
+{
+    [ServiceDefinitionMarker]
+    public interface IFileEqualityComparer : IServiceDefinition
+    {
+        Task Equals(string filePath1, string filePath2, IMessageSink messageSink);
+    }
+}
